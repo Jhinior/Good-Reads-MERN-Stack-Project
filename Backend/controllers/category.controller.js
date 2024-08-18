0dcab4f12a5df0ffffc10b7969c5e7b8bac5c072
Mahmoud Sayed <mahmoud.a.elsayed97@outlook.com>
2024-08-18 18:06:14 +0300

    add CRUD apis for admin user

--- Backend/controllers/category.controller.js
+++ Backend/controllers/category.controller.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wraper");
 const Category = require("../models/category.model");
-const AppError = require("../utils/appError");
+const appError = require("../utils/appError");
 const httpStatusText = require("../utils/httpStatusText");
 const { validationResult } = require("express-validator");
 const wordTransform = require("../utils/wordTransform");
@@ -16,12 +16,12 @@ const getCategory =   wrapAsync(async (req, res, next) => {
 const addCategory = wrapAsync(async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return next(new AppError (errors.array(), 400, httpStatusText.FAIL));
+      return next(new appError (errors.array(), 400, httpStatusText.FAIL));
     }
     let categoryName =  wordTransform(req.body.name);  
     const ifCategoryExist = await Category.findOne({ name: categoryName });
     if (ifCategoryExist) {
-      return next(new AppError(`Category ${ifCategoryExist.name} already exists`, 400 , httpStatusText.FAIL));
+      return next(new appError(`Category ${ifCategoryExist.name} already exists`, 400 , httpStatusText.FAIL));
     }
     const lastCategory = await Category.findOne().sort({ id: -1 });
     let newId = lastCategory? lastCategory.id + 1 : 1;
@@ -34,7 +34,7 @@ const addCategory = wrapAsync(async (req, res, next) => {
         const ifSaved = await category.save();
         res.status(201).json({ status: httpStatusText.SUCCESS, data:{ifSaved} });
     }catch(err){
-        return next(new AppError(err.message, 400, httpStatusText.FAIL))
+        return next(new appError(err.message, 400, httpStatusText.FAIL))
     }
 
   })
@@ -42,7 +42,7 @@ const addCategory = wrapAsync(async (req, res, next) => {
 const updateCategory = wrapAsync(async (req , res , next)=>{
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return next(new AppError (errors.array(), 400, httpStatusText.FAIL));
+      return next(new appError (errors.array(), 400, httpStatusText.FAIL));
     }
     const categoryId = +req.params.id;
     const newCategoryName =  wordTransform(req.body.name); 
@@ -50,7 +50,7 @@ const updateCategory = wrapAsync(async (req , res , next)=>{
       const updatedCategory = await Category.findOneAndUpdate({id:categoryId} ,{name: newCategoryName } , {new: true , runValidators: true});
       res.status(200).json({status: httpStatusText.SUCCESS , data:{updatedCategory}});
     }catch(err){
-      return next(new AppError(err.message, 400, httpStatusText.FAIL))
+      return next(new appError(err.message, 400, httpStatusText.FAIL))
     }    
 })
 
@@ -60,7 +60,7 @@ const deleteCategory = wrapAsync(async(req , res , next)=>{
      const ifDeleted = await Category.findOneAndDelete({id: categoryId});
      res.status(200).json({status: httpStatusText.SUCCESS , data:{ifDeleted}});
   }catch(err){
-      return next(new AppError(err.message, 400, httpStatusText.FAIL))
+      return next(new appError(err.message, 400, httpStatusText.FAIL))
   }
  
 })
