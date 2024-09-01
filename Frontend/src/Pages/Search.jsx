54527610702b318662e28ee1732aeab1ba251f26
ahmad <ahmadabdulmuniem8@gmail.com>
2024-09-01 18:02:30 +0300

    styling for the search page

--- Frontend/src/Pages/Search.jsx
+++ Frontend/src/Pages/Search.jsx
@@ -1,55 +1,66 @@
-
-import React, { useState, useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import "../Styles/Search.css";
 
 function useQuery() {
   return new URLSearchParams(useLocation().search);
 }
 
 function SearchResults() {
-  const params = new URLSearchParams(window.location.search);  ;
-  const searchQuery = params.get('searchQuery');
-  const [books,setBooks] = useState([])
-  const [authors,setAuthors] = useState([])
-  const [categories,setCategories] = useState([])
-  const [bookResults,setBookResults] = useState([])
-  const [authorResults,setAuthorResults] = useState([])
-  const [categoryResults,setCategoryResults] = useState([])
+  const params = new URLSearchParams(window.location.search);
+  const searchQuery = params.get("searchQuery");
+  const [books, setBooks] = useState([]);
+  const [authors, setAuthors] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [bookResults, setBookResults] = useState([]);
+  const [authorResults, setAuthorResults] = useState([]);
+  const [categoryResults, setCategoryResults] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState('');
- 
+  const [error, setError] = useState("");
+
   useEffect(() => {
     const fetchResults = async () => {
       console.log(searchQuery);
       if (searchQuery) {
         setLoading(true);
-        setError('');
+        setError("");
         try {
           // Fetch all the data concurrently
-          const [responseBooks, responseCategory, responseAuthor] = await Promise.all([
-            axios.get('http://localhost:5000/admin/book', { withCredentials: true }),
-            axios.get('http://localhost:5000/admin/category', { withCredentials: true }),
-            axios.get('http://localhost:5000/admin/author', { withCredentials: true }),
-          ]);
-  
+          const [responseBooks, responseCategory, responseAuthor] =
+            await Promise.all([
+              axios.get("http://localhost:5000/admin/book", {
+                withCredentials: true,
+              }),
+              axios.get("http://localhost:5000/admin/category", {
+                withCredentials: true,
+              }),
+              axios.get("http://localhost:5000/admin/author", {
+                withCredentials: true,
+              }),
+            ]);
+
           // Extract the data from the responses
           const fetchedBooks = responseBooks.data.data.books;
           const fetchedAuthors = responseAuthor.data.data.authors;
           const fetchedCategories = responseCategory.data.data.categories;
-  
+
           // Perform filtering before setting state
-          const filteredBooks = fetchedBooks.filter(book =>
+          const filteredBooks = fetchedBooks.filter((book) =>
             book.name.toLowerCase().includes(searchQuery.toLowerCase())
           );
-          const filteredAuthors = fetchedAuthors.filter(author =>
-            (author.firstName.toLowerCase() + " " + author.lastName.toLowerCase()).includes(searchQuery.toLowerCase())
+          const filteredAuthors = fetchedAuthors.filter((author) =>
+            (
+              author.firstName.toLowerCase() +
+              " " +
+              author.lastName.toLowerCase()
+            ).includes(searchQuery.toLowerCase())
           );
-          const filteredCategories = fetchedCategories.filter(category =>
+          const filteredCategories = fetchedCategories.filter((category) =>
             category.name.toLowerCase().includes(searchQuery.toLowerCase())
           );
-  
+
           // Update state
           setBooks(fetchedBooks);
           setAuthors(fetchedAuthors);
@@ -57,10 +68,9 @@ function SearchResults() {
           setBookResults(filteredBooks);
           setAuthorResults(filteredAuthors);
           setCategoryResults(filteredCategories);
-  
         } catch (err) {
-          console.error('Error fetching data:', err); // Debugging
-          setError('Error fetching search results');
+          console.error("Error fetching data:", err); // Debugging
+          setError("Error fetching search results");
         } finally {
           setLoading(false);
         }
@@ -68,76 +78,103 @@ function SearchResults() {
         setLoading(false);
       }
     };
-  
+
     fetchResults();
   }, [searchQuery]);
-  
-  
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{error}</p>;
   return (
-    <div>
-      <h2>Search Results for "{searchQuery}"</h2>
-      <h3>Books sreach results:</h3>
-      <div className="row">
-        {bookResults.map(book => (
-          <div className="col-md-4 mb-4" key={book.id}>
-            <div className="card mb-4 shadow-sm">
-              <img
-                src={`http://localhost:5000/uploads/${book.image}`}
-                className="card-img-top book-img"
-                alt={book.name}
-                onError={(e) => e.target.src = 'https://via.placeholder.com/350x600'}
-              />
-              <div className="card-body d-flex flex-column">
-                <h5 className="card-title text-truncate">{book.name}</h5>
-                <p className="card-text"><strong>Category:</strong> {book.category.name}</p>
-                <p className="card-text"><strong>Author:</strong> {book.author.firstName}</p>
-                <a href={`/books/${book.id}`} className="btn btn-primary mt-auto">View Details</a>
-              </div>
-            </div>
+    <div className="search-container">
+      <h2 className="head-search">Search Results for "{searchQuery}"</h2>
+      <h3 className="search-result">Books sreach results:</h3>
+      
+      <div className="book-search">
+  {bookResults.length > 0 ? (
+    bookResults.map((book) => (
+      <div className="search1" key={book.id}>
+        <div className="search2">
+          <img
+            src={`http://localhost:5000/uploads/${book.image}`}
+            className="card-img-top book-img"
+            alt={book.name}
+            onError={(e) =>
+              (e.target.src = "https://via.placeholder.com/350x600")
+            }
+          />
+          <div className="card-body d-flex flex-column">
+            <h5 className="card-title text-truncate">{book.name}</h5>
+            <p className="card-text">
+              <strong>Category:</strong> {book.category.name}
+            </p>
+            <p className="card-text">
+              <strong>Author:</strong> {book.author.firstName}
+            </p>
+            <a
+              href={`/books/${book.id}`}
+              className="btn btn-primary mt-auto"
+            >
+              View Details
+            </a>
           </div>
-        ))}
+        </div>
       </div>
+    ))
+  ) : (
+    <p>No Books were found!</p>
+  )}
+</div>
+
+      
+
       <hr></hr>
       <h3>Authors search results:</h3>
-      <div className="row">
-          {authorResults.length > 0 ? (
-            authors.map(author => (
-              <div className="col-md-4 mb-4" key={author.id}>
-                <div className="card category-card h-100 mb-4 shadow-sm">
-                  <div className="card-body d-flex flex-column">
-                  <h5 className="card-title">{author.firstName} {author.lastName}</h5>
+      <div className="search-authers">
+        {authorResults.length > 0 ? (
+          authors.map((author) => (
+            <div className="search-authers2" key={author.id}>
+              <div className="#">
+                <div className="#">
+                  <h5 className="card-title">
+                    {author.firstName} {author.lastName}
+                  </h5>
                   <img
-                       src={author.photo}
-                      className="card-img-top book-img"
-                      alt={author.firstName + author.lastName}
-                      onError={(e) => e.target.src = 'https://via.placeholder.com/350x600'}
-                    />
-                    {author.dob && (
-                      <p className="card-text">
-                        Born: {new Date(author.dob).toLocaleDateString()}
-                      </p>
-                    )}
-                    <Link to={`/author/${encodeURIComponent(author.firstName + ' ' + author.lastName)}`} className="btn btn-primary mt-auto">
-                      View Details
-                    </Link>
-                  </div>
+                    src={author.photo}
+                    className="card-img-top book-img"
+                    alt={author.firstName + author.lastName}
+                    onError={(e) =>
+                      (e.target.src = "https://via.placeholder.com/350x600")
+                    }
+                  />
+                  {author.dob && (
+                    <p className="card-text">
+                      Born: {new Date(author.dob).toLocaleDateString()}
+                    </p>
+                  )}
+                  <Link
+                    to={`/author/${encodeURIComponent(
+                      author.firstName + " " + author.lastName
+                    )}`}
+                    className="btn btn-primary mt-auto"
+                  >
+                    View Details
+                  </Link>
                 </div>
               </div>
-            ))
-          ) : ""
-          }
-        </div>
-        <hr></hr>
+            </div>
+          ))
+        ) : (
+          <p>No Authers were found!</p>
+        )}
+      </div>
+      <hr></hr>
       <h3>Categoreis search results:</h3>
-      <div className="container">
-        <div className="row">
+
+      {/* <div className="search-category">
         {categoryResults.map(category => (
-          <div className="col-md-4 mb-4" key={category._id}>
-            <div className="card category-card h-100 mb-4 shadow-sm">
-              <div className="card-body d-flex flex-column">
+          <div className="#" key={category._id}>
+            <div className="#">
+              <div className="#">
                 <h5 className="card-title">{category.name}</h5>
                 <Link to={`/category/${category.name}`} className="btn btn-primary mt-auto">
                   View Books
@@ -146,10 +183,28 @@ function SearchResults() {
             </div>
           </div>
         ))}
-        </div>
+        </div> */}
+      <div className="search-category">
+        {categoryResults.length > 0 ? (
+          categoryResults.map((category) => (
+            <div className="#" key={category._id}>
+              <div className="#">
+                <div className="#">
+                  <h5 className="card-title">{category.name}</h5>
+                  <Link
+                    to={`/category/${category.name}`}
+                    className="btn btn-primary mt-auto"
+                  >
+                    View Books
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>No categories found!</p>
+        )}
       </div>
-
-
     </div>
   );
 }
